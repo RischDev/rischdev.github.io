80ee1633d602f89c98420ba3086b25598171db7b
rischdev <risch1996@gmail.com>
2020-12-23 15:45:37 -0500

    Added localStorage for tracking previous progress. Added a reset button.

--- cardTracker/tracker.js
+++ cardTracker/tracker.js
@@ -1,9 +1,27 @@
+var foundArray;
+
 $(document).ready(function() {
 	CARDNAMES.forEach(generateCard);
+	
+	foundArray = JSON.parse(localStorage.getItem("foundArray"));
+	
+	if (foundArray == null) {
+		foundArray = [];
+	}
+	
+	foundArray.forEach(updateFound);
 });
 
 $(document).on("click", ".card", function() {
 	$(this).toggleClass("found");
+	
+	var id = $(this).attr("id");
+	if (foundArray.includes(id)) {
+		foundArray.splice(foundArray.indexOf(id), 1);
+	} else {
+		foundArray.push(id);
+	}
+	localStorage.setItem("foundArray", JSON.stringify(foundArray));
 });
 
 function generateCard(name, index) {
@@ -11,4 +29,16 @@ function generateCard(name, index) {
 			<div class="title">${CARDS[name] + 1}. ${name}</div>
 			<img class="icon" src="../images/${CARDS[name]}.png" />
 		</div>`);
+}
+
+function updateFound(id, index) {
+	$("#" + id).toggleClass("found");
+}
+
+function resetFound() {
+	$(".found").toggleClass("found");
+	
+	foundArray = [];
+	
+	localStorage.setItem("foundArray", JSON.stringify(foundArray));
 }
